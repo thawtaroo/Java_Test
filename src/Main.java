b2b491efd809769673bd9ff07e362717388d8cfc
TTO <myatthuta2019@gmail.com>
2023-07-21 23:06:29 +0630

    String

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,20 @@ import java.util.Date;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args){
-        Point point1 = new Point(10, 10);
-        Point point2 = point1;
-        point1.x = 20;
-        System.out.println(point2);
+//        Point point1 = new Point(10, 10);
+//        Point point2 = point1;
+//        point1.x = 20;
+//        System.out.println(point2);
+
+        String message = "Hello world " + "My Friend";
+        boolean isStartWithH =  message.startsWith("h");
+        int count = message.length();
+        String lowerCase = message.toLowerCase();
+        String upperCase = message.toUpperCase();
+
+        System.out.println(upperCase);
+        System.out.println(lowerCase);
+        System.out.println(message);
+        System.out.println(count);
     }
 }
