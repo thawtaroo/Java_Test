7e27acac4aceda27081553cb4cd51124d70152e0
TTO <myatthuta2019@gmail.com>
2023-07-21 22:42:48 +0630

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args){
+//        int snack = 100;
+//        snack = 50;
+//        long pt = 200;
+//        float money = 120.45F;
+//        double money2 = 234.56;
+//        String name = "TTO";
+//        Date date = new Date();
+//        System.out.println(date);
+
+//        byte x =100;
+//        byte y =x;
+//        x = 100;
+//        System.out.println(y);
+
+    }
+}
