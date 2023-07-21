7530d3a077895e2ee9411a9792c538293c7e7a30
TTO <myatthuta2019@gmail.com>
2023-07-21 22:49:01 +0630

    Point

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,13 @@
+import java.awt.*;
 import java.util.Date;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args){
-//        int snack = 100;
-//        snack = 50;
-//        long pt = 200;
-//        float money = 120.45F;
-//        double money2 = 234.56;
-//        String name = "TTO";
-//        Date date = new Date();
-//        System.out.println(date);
-
-//        byte x =100;
-//        byte y =x;
-//        x = 100;
-//        System.out.println(y);
-
+        Point point1 = new Point(10, 10);
+        Point point2 = point1;
+        point1.x = 20;
+        System.out.println(point2);
     }
 }
